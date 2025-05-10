7e8ecf2c4d72caa85ae56c567906889447114ab4
EugeniiPetrov <epoy74@gmail.com>
2025-05-10 09:13:39 +0500

    ыфмштп

---
+++ get_target_configuration_object.py
@@ -0,0 +1,71 @@
+# Напишите здесь описание скрипта
+"""
+<parameters>
+        <company>EugeniiPetrov</company>
+        <email>p174@mail.ru</email>
+        <title>Finding_appenned servers</title>
+        <version>1.0</version>
+</parameters>
+"""
+# objects_list("Channel")
+# Folder - класс родительских объектов ("Channels", "IP Devices", "Templates"),
+#  которым принадлежат все остальные классы;
+
+# Server - класс подключенных серверов;
+# IP Device - класс подключенных IP-устройств;
+# Channel - класс подключенных каналов;
+# GPIO Input - класс тревожных входов;
+# GPIO Output - класс тревожных выходов;
+# OperatorGUI - класс интерфейса оператора;
+# Template - класс шаблонов.
+
+#  Имя файла, куда пишем и режим его открытия
+PATH_AND_FILENAME_TO_WRITE = "/tmp/servers_list.txt"
+FILE_OPENING_MODE = "a"
+TARGET_COFINURATION_OBJECT = "Channel"
+
+message_to_user = message  #  noqa  #  type: ignore
+
+
+class FileErrorCustom(Exception):
+    """
+    Пробрасываю полученные ошибки далее
+    """
+
+    pass
+
+
+def main():
+    """
+    основной код программы
+    """
+    # objects_list is internal trassirs function
+    configuration_oblects = objects_list(TARGET_COFINURATION_OBJECT)  # noqa # type: ignore
+    try:
+        with open(
+            PATH_AND_FILENAME_TO_WRITE, FILE_OPENING_MODE
+        ) as file_for_info:
+            # pass
+            for index, enum_object_info in enumerate(
+                configuration_oblects, start=1
+            ):
+                info_of_object = "{}. {}\n".format(  #  noqa
+                    index, enum_object_info
+                )
+                # message_to_user(info_of_server)
+                file_for_info.write(info_of_object)
+    except OSError as err:
+        #  Написано для пайтона 2.7. там конструкции  raise ... from ... нет
+        #  и вызывает ошибку
+        #  Written for Python 2.7 — raise ... from ... syntax
+        #  is not supported and causes an error
+        raise (FileErrorCustom(err))  # noqa
+
+
+#  Это не подходит, так как это не запускается, как основной скрипт
+#  if __name__ == "__main__":
+"""
+Точка входа в программу
+"""
+message_to_user("Запуск системы логирования определенных настроек")
+main()
